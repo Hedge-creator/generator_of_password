57229b956d93d0d663160e004c63b45a58d6b07c
Hedge <85007266+Hedge-creator@users.noreply.github.com>
2022-08-30 21:12:23 +0300

    added code file

---
+++ generator of password.py
@@ -0,0 +1,24 @@
+import string
+import random
+#составляем список символом
+characters = list(string.ascii_letters + string.digits + "`~!@#$%^&*(){}[]:;\|?/,.<>-_=+")
+
+def generate_random_password():
+	#просим указать длину пароля
+	length = int(input("Длина требуемого пароля: "))
+	#перемешиваем символы
+	random.shuffle(characters)
+	#сохраняем пароль
+	password = []
+	for i in range(length):
+		password.append(random.choice(characters))
+	#подготавливаем результат
+	random.shuffle(password)
+	#конвертируем в иной формат
+	#выводим пароль
+	print("".join(password))
+
+
+
+#запускаем
+generate_random_password()
